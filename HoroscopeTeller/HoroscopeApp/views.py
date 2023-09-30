90c394129d9858ab6f79d94463e2783741e8892b
Bhavesh-Kapur <Bhavesh.105635@stu.upes.ac.in>
2023-09-30 11:54:45 +0530

    backend part to get the zodiac sign

--- HoroscopeTeller/HoroscopeApp/views.py
+++ HoroscopeTeller/HoroscopeApp/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render, HttpResponse, redirect
-
+import zodiac
 # Create your views here.
 
 def index(request):
+    if request.method=="POST":
+        name=request.POST.get('name')
+        dob=request.POST.get('dob')
+        print(dob)
+        print(type(dob))
+        z=zodiac.calculate_zodiac_sign(dob)
+
+        dic={"z":z}
+
+
+        return render(request,'page2.html',dic)
     return render(request,'index.html')
---
+++ HoroscopeTeller/zodiac.py
@@ -0,0 +1,39 @@
+import datetime
+
+def newuser(name,dob):
+    f = open("User.txt", "w")
+    f.write(name+'\n'+dob+'\n')
+    f.close()
+
+
+def calculate_zodiac_sign(dob):
+    month=int(dob[5:7])
+    day=int(dob[8:])
+
+    if (month == 1 and day >= 20) or (month == 2 and day <= 18):
+        return "Aquarius"
+    elif (month == 2 and day >= 19) or (month == 3 and day <= 20):
+        return "Pisces"
+    elif (month == 3 and day >= 21) or (month == 4 and day <= 19):
+        return "Aries"
+    elif (month == 4 and day >= 20) or (month == 5 and day <= 20):
+        return "Taurus"
+    elif (month == 5 and day >= 21) or (month == 6 and day <= 20):
+        return "Gemini"
+    elif (month == 6 and day >= 21) or (month == 7 and day <= 22):
+        return "Cancer"
+    elif (month == 7 and day >= 23) or (month == 8 and day <= 22):
+        return "Leo"
+    elif (month == 8 and day >= 23) or (month == 9 and day <= 22):
+        return "Virgo"
+    elif (month == 9 and day >= 23) or (month == 10 and day <= 22):
+        return "Libra"
+    elif (month == 10 and day >= 23) or (month == 11 and day <= 21):
+        return "Scorpio"
+    elif (month == 11 and day >= 22) or (month == 12 and day <= 21):
+        return "Sagittarius"
+    else:
+        return "Capricorn"
+
+
+
